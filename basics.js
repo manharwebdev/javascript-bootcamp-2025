e1416c4e1e5a9f1b47ec3a4c72746a951584968a
Manhar Hadiyal <manharwebdev@gmail.com>
2025-12-14 15:43:52 +0530

    refactor: converted if-else to ternary operators

--- basics.js
+++ basics.js
@@ -8,18 +8,30 @@ const message = `Hello, My Name is ${firstName} ${lastName}. I am ${age} years o
 console.log(message);
 
 // Checking age for voter eligibility
-if (age >= 18) {
-    console.log("You are eligible to vote.");
-} else {
-    console.log("You are not eligible to vote.");
-}
+// if (age >= 18) {
+//     console.log("You are eligible to vote.");
+// } else {
+//     console.log("You are not eligible to vote.");
+// }
 
 // checking user role if admin then show message you can edit product and if guest then view product only
-const userRole = "guest"; // Change to "guest" to test the other condition
-if (userRole === "admin") {
-    console.log("Welcome Admin. You can manage the Inventory.");
-} else if (userRole === "guest") {
-    console.log("Welcome Guest. You can only view products.");
-}   else {
-    console.log("Access Denied.");
-}
+const userRole = "admin"; // Change to "guest" to test the other condition
+// if (userRole === "admin") {
+//     console.log("Welcome Admin. You can manage the Inventory.");
+// } else if (userRole === "guest") {
+//     console.log("Welcome Guest. You can only view products.");
+// }   else {
+//     console.log("Access Denied.");
+// }
+
+// i want to convert above code into ternary operator
+userRole === "admin"
+    ? console.log("Welcome Admin. You can manage the Inventory.")
+    : userRole === "guest"
+    ? console.log("Welcome Guest. You can only view products.")
+    : console.log("Access Denied.");
+
+    age >= 18 ? console.log("You are eligible to vote.") : console.log("You are not eligible to vote.");
+
+    const voterStatus = age >= 18 ? "eligible to vote." : "not eligible to vote.";
+    console.log("You are", voterStatus);

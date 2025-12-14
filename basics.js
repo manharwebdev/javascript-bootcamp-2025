0adbfcb442af808d6cd6b184e70e3c3944282db7
Manhar Hadiyal <manharwebdev@gmail.com>
2025-12-14 14:48:34 +0530

    Day 1: Mastered Variables and Template Literals

---
+++ basics.js
@@ -0,0 +1,7 @@
+const currentYear = 2025;
+const birthYear = 1992;
+let age = currentYear - birthYear;
+console.log("Age:", age);
+const firstName = "Manhar";
+const lastName = "Hadiyal";
+const message = `Hello, My Name is ${firstName} ${lastName}. I am ${age} years old.`;console.log(message);

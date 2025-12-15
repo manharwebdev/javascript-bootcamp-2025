bcd89c643529033756878b66a3eefe1100646c0e
Manhar Hadiyal <manharwebdev@gmail.com>
2025-12-15 20:48:17 +0530

    message

---
+++ loops.js
@@ -0,0 +1,9 @@
+// 1. Same Inventory
+let inventory = ["Laptop", "Mouse", "Keyboard", "Monitor", "Printer"];
+
+// 2. The Loop
+// "i" stands for Index
+for (let i = 0; i < inventory.length; i++) {
+    // This code runs 5 times automatically
+    console.log(`Item number ${i}: ${inventory[i]}`);
+}

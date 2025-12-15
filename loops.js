d30a8c7ee60bf410c1e736dbc640cb09ece6bf71
Manhar Hadiyal <manharwebdev@gmail.com>
2025-12-15 20:50:54 +0530

    message

--- loops.js
+++ loops.js
@@ -1,9 +1,16 @@
-// 1. Same Inventory
-let inventory = ["Laptop", "Mouse", "Keyboard", "Monitor", "Printer"];
+// 1. The Cart (Prices)
+let prices = [200, 500, 1500, 800, 3000, 100];
 
-// 2. The Loop
-// "i" stands for Index
-for (let i = 0; i < inventory.length; i++) {
-    // This code runs 5 times automatically
-    console.log(`Item number ${i}: ${inventory[i]}`);
+// 2. The Register (Start at 0)
+let total = 0;
+
+// 3. The Scan (Loop)
+for (let i = 0; i < prices.length; i++) {
+    total = total + prices[i]; // Add current price to the total
+    // Visualization (Optional - helps you see it happen)
+    console.log(`Adding ${prices[i]}, new total is: ${total}`);
 }
+
+// 4. The Receipt
+console.log("--------------------");
+console.log(`Final Total Bill: ₹${total}`);
